bbdad638a79c7406b9a018ce65cea0316e50a073
qlghwp123 <qlghwp123@naver.com>
2022-08-24 16:29:36 +0900

    Add 0824 DB practice

--- db/models.py
+++ db/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+class Director(models.Model):
+    name = models.TextField()
+    debut = models.DateTimeField(null=True)
+    country = models.TextField()
+
+    def print(self):
+        print(self.name, self.debut, self.country)
+
+class Genre(models.Model):
+    title = models.TextField(null=True)
