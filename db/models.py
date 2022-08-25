19b447a017ed221f65d147d7348349ee8d5eb607
qlghwp123 <qlghwp123@naver.com>
2022-08-25 18:26:49 +0900

    Add 0825 Django ORM practice

--- db/models.py
+++ db/models.py
@@ -2,11 +2,45 @@ from django.db import models
 
 class Director(models.Model):
     name = models.TextField()
-    debut = models.DateTimeField(null=True)
+    debut = models.DateTimeField()
     country = models.TextField()
 
     def print(self):
-        print(self.name, self.debut, self.country)
+        print("id :", self.id)
+        print("name :", self.name)
+        print("debut :", self.debut)
+        print("country :", self.country)
 
 class Genre(models.Model):
-    title = models.TextField(null=True)
+    title = models.TextField()
+
+    def print(self):
+        print("id :", self.id)
+        print("title :", self.title)
+
+class Movie(models.Model):
+    director = models.ForeignKey(Director,on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre,on_delete=models.CASCADE)
+    title = models.TextField()
+    opening_date = models.DateField()
+    running_time = models.IntegerField()
+    screening = models.BooleanField()
+
+    def __init__(self):
+        super()
+
+    def __init__(self, _director, _genre, _title, _opening_date, _running_time, _screening):
+        self.director = Director.objects.get(name=_director)
+        self.genre = Genre.objects.get(title=_genre)
+        self.title = _title
+        self.opening_date = _opening_date
+        self.running_time = _running_time
+        self.screening = _screening
+
+    def print(self):
+        print(self.director, 
+                self.genre, 
+                self.title, 
+                self.opening_date,
+                self.screening
+            )
